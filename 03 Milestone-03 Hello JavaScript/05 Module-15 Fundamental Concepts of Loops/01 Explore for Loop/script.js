f27e00ef64149180732b0dffac21147f2e9fcd08
Hanif <mohammadabuhanif95@gmail.com>
2026-01-27 06:42:12 +0800

    practice loops, and basic conditionals
    - Recap const, let, and variable naming
    - Use for...of loops to iterate over arrays of numbers and fruits
    - Implement if conditions and while loops with basic logic

---
+++ 03 Milestone-03 Hello JavaScript/05 Module-15 Fundamental Concepts of Loops/01 Explore for Loop/script.js
@@ -0,0 +1,12 @@
+const numbers = [12, 64, 34, 65, 87, 23];
+console.log(numbers);
+
+for (const num of numbers) {
+  console.log(num);
+}
+
+const fruits = ["orange", "apple", "mango", "watermelon", "jackfruit"];
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("I want to eat");
+}
---
+++ 03 Milestone-03 Hello JavaScript/05 Module-15 Fundamental Concepts of Loops/02 Introduction to While Loop/script.js
@@ -0,0 +1,12 @@
+const number = 8;
+if (number < 10) {
+  console.log("small number");
+}
+console.log("Something new is cooking");
+
+// =========> While Loop <==============
+let num = 0;
+while (num < 5) {
+  console.log("loop", num);
+  num = num + 1;
+}
