783211c284f4a81ebf6676404f680e2bed59113f
Hanif <mohammadabuhanif95@gmail.com>
2026-02-02 09:51:05 +0800

    - Demonstrate Math object methods (min, max, PI, abs, round, floor, ceil, random) - Show Date object creation, manipulation, and localization - Implement variable swapping using both temporary variable and destructuring

---
+++ 03 Milestone-03 Hello JavaScript/11 Module-18 JavaScript Simple Coding Problems(Part-1)/05_math.js
@@ -0,0 +1,34 @@
+/*
+const Math = {
+    min : function min(num1, num2){
+        return num2;
+    }
+}
+
+console.log(Math.min);
+console.log(Math.max);
+*/
+// const min = Math.min
+
+const min = Math.min(23, 1, 4, 6, 24, 76, 57, 90, -23);
+console.log(min);
+
+const max = Math.max(23, 1, 4, 6, 24, 76, 57, 90, -23);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5 - 10));
+
+console.log(Math.round(23.4));
+console.log(Math.round(23.85));
+
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.99));
+
+console.log(Math.ceil(4.99));
+console.log(Math.ceil(4.0001));
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
