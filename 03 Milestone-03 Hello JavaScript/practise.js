8884a8f5a77340b4ba426a2027204e369eebbb85
Hanif <mohammadabuhanif95@gmail.com>
2026-02-05 17:24:03 +0800

    Delete the unnecessary folder and changes all the file name

---
+++ 03 Milestone-03 Hello JavaScript/practise.js
@@ -0,0 +1,28 @@
+var price=34;
+price = price + 10;
+console.log(price);
+
+price+=34;
+console.log(price);
+
+var fridge = 34000;
+
+fridge = fridge - 1000;
+console.log(fridge);
+
+fridge -= 1000;
+console.log(fridge);
+
+var orangePrice = 30;
+orangePrice = orangePrice * 2;
+console.log(orangePrice);
+
+orangePrice *= 2;
+console.log(orangePrice);
+
+var mangoPrice = 50;
+mangoPrice = mangoPrice / 2;
+console.log(mangoPrice);
+
+mangoPrice /= 2;    
+console.log(mangoPrice);
