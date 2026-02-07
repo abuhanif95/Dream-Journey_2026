75725ab0f3cd85e71134f2e4f34f32d1d171d8b7
Hanif <mohammadabuhanif95@gmail.com>
2026-02-08 06:36:34 +0800

    complete the Password Strength Checker problem

---
+++ 03 Milestone-03 Hello JavaScript/13 Practise Day Conceptual session/04_problem03.js
@@ -0,0 +1,65 @@
+/*
+==> Problem 3: Password Strength Checker
+    Function name: checkPassword(password)
+
+    Rules:
+    Length must be at least 8
+    Must contain at least 1 number
+    Must contain at least 1 uppercase letter
+    Must not contain spaces
+
+    Test case 1
+
+    Input:  "helloWorld"
+    Output:
+    { valid: false, reasons: ["missing number"] }
+
+    Test case 2
+
+    Input:  "Hello123"
+    Output: { valid: true, reasons: [] }
+*/
+
+function checkPassword(password) {
+  let reason = [];
+  let length = password.length;
+  let hasUpperCase = false;
+  let hasNumber = false;
+  let hasSpace = password.includes(" ");
+
+  for (let i = 0; i < length; i++) {
+    let character = password[i];
+    if (character >= "0" && character <= "9") {
+      hasNumber = true;
+    }
+    if (character >= "A" && character <= "Z") {
+      hasUpperCase = true;
+    }
+  }
+
+  if (hasNumber === false) {
+    reason.push("Missing Number");
+  }
+  if (hasUpperCase === false) {
+    reason.push("Missing Uppercase Character");
+  }
+
+  if (hasSpace === true) {
+    reason.push("Space Found !!");
+  }
+
+  let isValid;
+  if (reason.length === 0) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
+  return {
+    valid: isValid,
+    reason,
+  };
+}
+
+let output = checkPassword("Sfthanif24");
+console.log(output);
